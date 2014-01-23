b3feb271c4eac29dbd0fc62e46e77f602edac986
Parker Lusk <parkerclusk@gmail.com>
2014-01-23 15:57:26 -0700

    added Restangular and http token auth headers

--- ro-frontend/public/js/app.js
+++ ro-frontend/public/js/app.js
@@ -9,7 +9,7 @@ var roApp = angular.module('roApp', [
         'roApp.filters',
         'roApp.directives'
     ])
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
         $routeProvider
             .when('/home', {
                 templateUrl: 'partials/home.tpl.html',
@@ -19,8 +19,10 @@ var roApp = angular.module('roApp', [
             .otherwise({
                 redirectTo: '/home'
             });
+
+            RestangularProvider.setBaseUrl('http://localhost:8001');
     }])
-    .run(['$location', '$rootScope', 'baseTitle', function ($location, $rootScope, baseTitle) {
+    .run(['$location', '$rootScope', 'baseTitle', '$http', 'Restangular', function ($location, $rootScope, baseTitle, $http, RestangularProvider) {
         $rootScope.$on('$routeChangeSuccess', function (event, current) {
             // Check to see if the 'title' attribute exists on the route
             if (current.hasOwnProperty('$route')) {
@@ -28,5 +30,24 @@ var roApp = angular.module('roApp', [
             } else {
                 $rootScope.title = baseTitle.substring(0, baseTitle.length - 3);
             }
+
+        /* The following is for $http and Restangular token auth */
+        if (SessionService.isLoggedIn()) {
+            var token = SessionService.getSession();
+            $http.defaults.headers.common['Authorization'] = 'Token ' + token;
+        }
+
+
+        // add Auth Token to every Restangular request
+        RestangularProvider.setFullRequestInterceptor(function(element, operation, route, url, headers, params) {
+            if (SessionService.isLoggedIn()) {
+                var token = SessionService.getSession();
+                headers['Authorization'] = 'Token ' + token;
+            }
+
+            return { element: element, params: params, headers: headers }
+            });
         });
+
+
     }]);
