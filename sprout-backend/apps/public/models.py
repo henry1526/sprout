1e8154a0ea01cd8ea7ca66a3f23392ac76b908a3
Parker Lusk <parkerclusk@gmail.com>
2014-04-18 15:51:50 -0600

    setting more things up...

--- sprout-backend/apps/public/models.py
+++ sprout-backend/apps/public/models.py
@@ -4,6 +4,15 @@ from rest_framework.authtoken.models import Token
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+###
+### The below @receiever is a Python decorator. Decorators can alter the way
+### that methods work. This one in particular listens for anytime a User object is
+### saved, and if it is just being created, it creates a new security token associated
+### with that user. These tokens are used for the REST Framework, and allow us to 
+### track who is logged in and allow different permissions per user
+###
+###
+
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     ''' Creates a token whenever a User is created '''
@@ -11,15 +20,23 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
         Token.objects.create(user=instance)
 
 
-class Address(models.Model):
-    ''' Model features for an address '''
-    street = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    state = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
+###
+### Below is an example of a model. Uncomment to see it in action.
+### When you create a new model, you must create a new schema migration
+### [ python manage.py schemamigration __YOUR-APP-NAME__ --auto ]
+### Once the migration is created, you then have to apply it with:
+### [ python manage.py migrate ]
+###
 
-    def __unicode__(self):
-        return u'%s, %s, %s' % (self.street, self.city, self.state)
+# class Address(models.Model):
+#     ''' Model features for an address '''
+#     street = models.CharField(max_length=200)
+#     city = models.CharField(max_length=200)
+#     state = models.CharField(max_length=200)
+#     country = models.CharField(max_length=200)
 
-    class Meta:
-        verbose_name_plural = 'Address'
+#     def __unicode__(self):
+#         return u'%s, %s, %s' % (self.street, self.city, self.state)
+
+#     class Meta:
+#         verbose_name_plural = 'Address'
